61cfcf71d5dff7fd6a3cb57c2442e29f6622b459
Antasjain <antasjain2004@gmail.com>
2025-03-17 13:30:39 +0530

    Fixed document

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -40,15 +40,9 @@ export const BackgroundGradientAnimation = ({
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
   useEffect(() => {
-  if (typeof document !== "undefined") {
-    document.body.style.setProperty(
-      "--gradient-background-start",
-      gradientBackgroundStart
-    );
-    document.body.style.setProperty(
-      "--gradient-background-end",
-      gradientBackgroundEnd
-    );
+  if (typeof window !== "undefined") {
+    document.body.style.setProperty("--gradient-background-start", gradientBackgroundStart);
+    document.body.style.setProperty("--gradient-background-end", gradientBackgroundEnd);
     document.body.style.setProperty("--first-color", firstColor);
     document.body.style.setProperty("--second-color", secondColor);
     document.body.style.setProperty("--third-color", thirdColor);
@@ -59,6 +53,7 @@ export const BackgroundGradientAnimation = ({
     document.body.style.setProperty("--blending-value", blendingValue);
   }
 }, []);
+
 
   useEffect(() => {
     function move() {
